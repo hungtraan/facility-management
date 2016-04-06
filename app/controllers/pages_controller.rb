de55bf911daf81d2e4aa433194b65e82e6ad8529
Hùng Trần <hung.tranviet@tiki.vn>
2016-04-05 23:28:25 -0400

    Working student site, showing only number of occupancy, no scheduler yet

---
+++ app/controllers/pages_controller.rb
@@ -0,0 +1,31 @@
+class PagesController < ApplicationController
+def student
+      mysql_host = 'us-cdbr-iron-east-03.cleardb.net'
+      mysql_username = 'bbaf414965fded'
+      mysql_password = '2f3310e8'
+      mysql_db_name = 'heroku_fa3c47157b8ffc1'
+      db = Mysql2::Client.new(:host => mysql_host, :username => mysql_username, :password => mysql_password, :port => 3306, :database => mysql_db_name, :flags => Mysql2::Client::MULTI_STATEMENTS )
+    sql = "select occupancy
+    from 
+    (SELECT max(time_to), occupancy FROM occupancies) most_recent
+     "
+    results = ''
+
+    #output = []
+
+    results = db.query(sql)
+    results.each do |row|
+      # conveniently, row is a hash
+      # the keys are the fields, as you'd expect
+      # the values are pre-built ruby primitives mapped from their corresponding field types in MySQL
+      @occupancy = row["occupancy"]
+      puts row["occupancy"] # row["id"].class == Fixnum
+      if row["dne"]  # non-existant hash entry is nil
+      puts row["dne"]
+      end
+    end
+    
+    # puts "Latest Occupancy: " + results
+  end
+
+end
--- config/routes.rb
+++ config/routes.rb
@@ -4,6 +4,11 @@ Rails.application.routes.draw do
 
   # You can have the root of your site routed with "root"
   # root 'welcome#index'
+  # root 'application#hello'
+  root 'pages#student'
+
+  get '/bye' => 'application#bye'
+
 
   # Example of regular route:
   #   get 'products/:id' => 'catalog#view'
