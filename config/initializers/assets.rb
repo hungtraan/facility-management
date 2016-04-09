bf6ece93ae9969fce365ca7d68f27322bbef061f
Hùng Trần <hung.tranviet@tiki.vn>
2016-04-08 22:20:53 -0400

    Update js css

--- config/initializers/assets.rb
+++ config/initializers/assets.rb
@@ -2,7 +2,7 @@
 
 # Version of your assets, change this if you want to expire all your assets.
 Rails.application.config.assets.version = '1.0'
-Rails.application.config.assets.precompile += %w( student.js )
+Rails.application.config.assets.precompile += %w( student.js dashboard_realtime.js pages.js dashboard_realtime.scss student.scss )
 # Add additional assets to the asset load path
 # Rails.application.config.assets.paths << Emoji.images_path
 
