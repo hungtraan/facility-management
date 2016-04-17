d2f7c66a4d129dfcf7883c30ae66850c4aa184f2
Mulin <tang_m5@denison.edu>
2016-04-17 16:48:09 -0400

    Revert to 4a25089f3e3787

--- config/routes.rb
+++ config/routes.rb
@@ -1,6 +1,4 @@
 Rails.application.routes.draw do
-  get 'sessions/new'
-
   # The priority is based upon order of creation: first created -> highest priority.
   # See how all your routes lay out with "rake routes".
 
